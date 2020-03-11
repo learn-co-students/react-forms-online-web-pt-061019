e54db55cb0e01f827c5c947dc2cf591c44ea9f09
Khris Punzalan <khrispunzalan@gmail.com>
2020-03-11 05:25:55 -0700

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,31 +1,110 @@
 import React from 'react';
-
+ 
 class Form extends React.Component {
-  state = {
-    firstName: "John",
-    lastName: "Henry"
-  }
-
-  handleFirstNameChange = event => {
-    this.setState({
-      firstName: event.target.value
-    })
-  }
-
-  handleLastNameChange = event => {
-    this.setState({
-      lastName: event.target.value
-    })
-  }
-
   render() {
     return (
-      <form>
-        <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
-        <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
-      </form>
+      <div>
+        <form>
+        {/* REFACTORED */}
+        <input
+            type="text"
+            name="firstName"
+            onChange={event => this.props.handleChange(event)}
+            value={this.props.formData.firstName}
+          />
+          <input
+            type="text"
+            name="lastName"
+            onChange={event => this.props.handleChange(event)}
+            value={this.props.formData.lastName} />
+
+          {/* <input
+            type="text"
+            onChange={event => this.props.handleFirstNameChange(event)}
+            value={this.props.formData.firstName}
+          />
+          <input
+            type="text"
+            onChange={event => this.props.handleLastNameChange(event)}
+            value={this.props.formData.lastName}
+          /> */}
+        </form>
+      </div>
     )
   }
 }
-
+ 
 export default Form;
+
+
+
+
+
+// BEFORE:
+
+// import React from 'react';
+ 
+// class Form extends React.Component {
+//   state = {
+//     firstName: "John",
+//     lastName: "Henry",
+//     submittedData: []
+//   }
+ 
+//   handleFirstNameChange = event => {
+//     this.setState({
+//       firstName: event.target.value
+//     })
+//   }
+ 
+//   handleLastNameChange = event => {
+//     this.setState({
+//       lastName: event.target.value
+//     })
+//   }
+ 
+//   // Controlled form
+//   handleSubmit = event => {
+//     event.preventDefault()
+//     let formData = { firstName: this.state.firstName, lastName: this.state.lastName }
+//     let dataArray = this.state.submittedData.concat(formData)
+//     this.setState({submittedData: dataArray})
+//   }
+
+// //  Uncontrolled form
+// //   handleSubmit = event => {
+// //   event.preventDefault()
+// //   const firstName = event.target.children[0].value
+// //   const lastName = event.target.children[1].value
+// //   this.sendFormDataSomewhere({ firstName, lastName })
+// // }
+ 
+//   listOfSubmissions = () => {
+//     return this.state.submittedData.map(data => {
+//       return <div><span>{data.firstName}</span> <span>{data.lastName}</span></div>
+//     })
+//   }
+ 
+//   render() {
+//     return (
+//       <div>
+//         <form onSubmit={event => this.handleSubmit(event)}>
+//           <input
+//             type="text"
+//             onChange={event => this.handleFirstNameChange(event)}
+//             value={this.state.firstName}
+//           />
+//           <input
+//             type="text"
+//             onChange={event => this.handleLastNameChange(event)}
+//             value={this.state.lastName}
+//           />
+//           <input type="submit"/>
+//         </form>
+//         {this.listOfSubmissions()}
+//       </div>
+//     )
+//   }
+// }
+ 
+// export default Form;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Form from './components/Form'
-
+import ParentComponent from './components/ParentComponent';
+ 
 ReactDOM.render(
   <div>
-    <Form />
+    <ParentComponent/>
   </div>,
   document.getElementById('root')
 );
+
+// BEFORE:
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Form from './components/Form'
+
+// ReactDOM.render(
+//   <div>
+//     <Form />
+//   </div>,
+//   document.getElementById('root')
